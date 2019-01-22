efa4d72ae4a8149489f0fc1bd1d32df0a1292168
yannikmoog <ymoog@icloud.com>
2019-01-22 16:25:39 +0100

    updated java version

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -1,8 +1,8 @@
 install_java() {
   local version="$1"
   local dir="$2"
-  
-  local download_url="https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-8u201-linux-x64.tar.gz"
+  local download_url="http://download.oracle.com/otn-pub/java/jdk/11.0.2+9/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_linux-x64_bin.tar.gz"
+  local download_url_old="https://download.oracle.com/otn-pub/java/jdk/8u201-b09/42970487e3af4f5aa5bca3f542482c60/jdk-8u201-linux-x64.tar.gz"
   echo "Downloading JAVA [$download_url]"
   curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
   echo "Download complete!"
@@ -12,7 +12,7 @@ install_java() {
   mkdir $dir
   tar xzf /tmp/java.tar.gz -C /tmp/jdk
   rm -rf $dir/*
-  mv /tmp/jdk/jdk1.8.0_201/* $dir
+  mv /tmp/jdk/jdk11.0.2/* $dir
   chmod +x $dir/bin
   echo "Installation complete!"	
 }
